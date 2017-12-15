2ade97a6f9d9d0b873e10f5ad608ffba88847fe1
Mike Ekert <mikeekert@gmail.com>
2017-12-15 15:45:09 -0600

    captcha in place

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,7 +3,7 @@ var url = require('url');
 var config = {
     user: process.env.RDS_USERNAME || null, //env var: PGUSER
     password: process.env.RDS_PASSWORD || null, //env var: PGPASSWORD
-    host: process.env.RDS_HOSTNAME || 'localhost', // Server hosting the postgres database
+    host: process.env.RDS_HOSTNAME || 'localhost', // Server
     port: process.env.RDS_PORT || 5432, //env var: PGPORT
     database: process.env.RDS_DB_NAME || 'mn-senate', //env var: PGDATABASE
     max: 10, // max number of clients in the pool
