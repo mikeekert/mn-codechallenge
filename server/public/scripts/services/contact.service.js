2b640bbfbabfebb12cd06884dc8a2ab29a1422cc
Mike Ekert <mikeekert@gmail.com>
2017-12-15 14:59:08 -0600

    testing validation

--- server/public/scripts/services/contact.service.js
+++ server/public/scripts/services/contact.service.js
@@ -2,28 +2,32 @@ myApp
     .factory('ContactService', function (vcRecaptchaService, $http) {
 
         var userObject = [{}];
-        
+
         return {
             userObject: userObject, 
 
             formsend: (user) => { // send user data to server
                 userObject.capchaMessage = ''; // clear capcha message on send
-                const post_data = { //prepare payload
-                    'fName': user.fName,
-                    'lName': user.lName,
-                    'phone': user.phone,
-                    'address': user.address,
-                    'email': user.email,
-                    'comments': user.comments,
-                    'g-recaptcha-response': vcRecaptchaService.getResponse() // grab token from form
-                };
 
-                $http 
-                    .post('/contact/', post_data) // post to server API
-                    .then((response) => {
-                        console.log(response.data.responseDesc);
-                        userObject.capchaMessage = response.data.responseDesc; // return captcha json message
-                    });
+                if (!grecaptcha.getResponse().length) {
+                    alert('complete the captcha');
+                } else {
+                    const post_data = { //prepare payload
+                        'fName': user.fName,
+                        'lName': user.lName,
+                        'phone': user.phone,
+                        'address': user.address,
+                        'email': user.email,
+                        'comments': user.comments,
+                        'g-recaptcha-response': vcRecaptchaService.getResponse() // grab token from form
+                    };
+    
+                    $http 
+                        .post('/contact/', post_data) // post to server API
+                        .then((response) => {
+                            userObject.capchaMessage = response.data.responseDesc; // return captcha json message
+                        });
+                }
             }
         };
     });
