8be937b697589aa96f0e43b0e1de8f771fb84735
Mike Ekert <mikeekert@gmail.com>
2017-12-15 14:25:53 -0600

    test

--- server/public/scripts/services/contact.service.js
+++ server/public/scripts/services/contact.service.js
@@ -11,7 +11,7 @@ myApp
                     'comments': user.comments,
                     'g-recaptcha-response': vcRecaptchaService.getResponse()
                 };
-                console.log(user);
+                console.log(post_data);
 
                 // $http
                 //     .post('/contact/', post_data)
