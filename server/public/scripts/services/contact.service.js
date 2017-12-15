791d00f25c5776064c603b44fa4cd3319bb5f16e
Mike Ekert <mikeekert@gmail.com>
2017-12-15 14:19:24 -0600

    test

--- server/public/scripts/services/contact.service.js
+++ server/public/scripts/services/contact.service.js
@@ -2,19 +2,19 @@ myApp
     .factory('ContactService', function (vcRecaptchaService, $http) {
         return {
             formsend: (user) => {
-                var post_data = { //prepare payload for request
-                    'fName': user.fName,
-                    'lName': user.lName,
-                    'phone': user.phone,
-                    'address': user.address,
-                    'email': user.email,
-                    'comments': user.comments,
-                    'g-recaptcha-response': vcRecaptchaService.getResponse()
-                };
-                console.log(user);
+                // var post_data = { //prepare payload for request
+                //     'fName': user.fName,
+                //     'lName': user.lName,
+                //     'phone': user.phone,
+                //     'address': user.address,
+                //     'email': user.email,
+                //     'comments': user.comments,
+                //     'g-recaptcha-response': vcRecaptchaService.getResponse()
+                // };
+                // console.log(user);
 
                 $http
-                    .post('/contact/', post_data)
+                    .post('/contact/', user)
                     .then((response) => {
 
                     });
