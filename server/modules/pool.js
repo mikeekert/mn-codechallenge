ac90e4e1bc2cba47153c0b798f74b0185d6181c1
Mike Ekert <mikeekert@gmail.com>
2017-12-18 11:21:23 -0600

    heroku/local testing

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,15 +1,32 @@
 var pg = require('pg');
 var url = require('url');
-var config = {
-    user: process.env.PGUSER || null, //env var: PGUSER
-    password: process.env.PGPASSWORD || null, //env var: PGPASSWORD
-    host: process.env.PGSERVER || 'localhost', // Server
-    port: process.env.PGPORT || 5432, //env var: PGPORT
-    database: process.env.PGDATABASE || 'mn-senate', //env var: PGDATABASE
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-};
+
+if (process.env.DATABASE_URL) {
+    var params = url.parse(process.env.DATABASE_URL);
+    var auth = params.auth.split(':');
+  
+    config = {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true, // heroku requires ssl to be true
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+  
+  } else {
+    config = {
+      user: process.env.RDS_USERNAME || null, //env var: PGUSER
+      password: process.env.RDS_PASSWORD || null, //env var: PGPASSWORD
+      host: process.env.RDS_HOSTNAME || 'localhost', // Server hosting the postgres database
+      port: process.env.RDS_PORT || 5432, //env var: PGPORT
+      database: process.env.RDS_DB_NAME || 'mn-senate', //env var: PGDATABASE
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+  }
+  
 
 module.exports = new pg.Pool(config);
