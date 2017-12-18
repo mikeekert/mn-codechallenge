f49e1b3153adaebbdb1e4c50ad2a33b7a2299b21
Mike Ekert <mikeekert@gmail.com>
2017-12-18 12:42:25 -0600

    updated obj sent to server and server receiving route

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -6,7 +6,7 @@ const path = require('path');
 const nodemailer = require('nodemailer');
 
 router.post('/', (req, res) => {
-    console.log(req.body.data);
+    console.log('data in /get route', req.body.data);
     const user = req.body;
     const secret = process.env.V2PASS;
 
