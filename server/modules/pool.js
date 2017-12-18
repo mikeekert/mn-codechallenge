ee439dfa7764970e5df5fa87fbba192abf4e4127
Mike Ekert <mikeekert@gmail.com>
2017-12-18 11:09:25 -0600

    pg heroku fix

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,15 @@
 var pg = require('pg');
 var url = require('url');
 var config = {
-    user: process.env.RDS_USERNAME || null, //env var: PGUSER
-    password: process.env.RDS_PASSWORD || null, //env var: PGPASSWORD
-    host: process.env.RDS_HOSTNAME || 'localhost', // Server
-    port: process.env.RDS_PORT || 5432, //env var: PGPORT
-    database: process.env.RDS_DB_NAME || 'mn-senate', //env var: PGDATABASE
+    user: process.env.PGUSER || null, //env var: PGUSER
+    password: process.env.PGPASSWORD || null, //env var: PGPASSWORD
+    host: process.env.PGSERVER || 'localhost', // Server
+    port: process.env.PGPORT || 5432, //env var: PGPORT
+    database: process.env.PGDATABASE || 'mn-senate', //env var: PGDATABASE
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 };
 
 module.exports = new pg.Pool(config);
